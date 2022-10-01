7fdc67b19e426201f8de31979e7daf671d737016
rkran <rkranjith03@gmail.com>
2022-10-01 17:09:19 +0530

    Initial Commit

--- Amazon.py
+++ Amazon.py
@@ -32,21 +32,4 @@ class Amazon_Price:
         self.price=int(self.price)
         self.driver.close()
 
-    def update(self,users,db):
-        for user in users:
-            for item in user.items:
-                self.driver.get(url=item.url)
-                price_tag = self.driver.find_element(By.CLASS_NAME, "a-price-whole")
-                self.price = price_tag.text.replace(",", "")
-                self.price = int(self.price)
-                self.driver.close()
-                item.price=self.price
-                db.session.commit()
-                if self.price < int(item.low_price):
-                    item.low_price = self.price
-                    db.session.commit()
-                    email = Send_Email(email=user.email, user=user, item=item)
-                    email.low_price()
-                if self.price < int(item.budget):
-                    email = Send_Email(email=user.email, user=user, item=item)
-                    email.budget()
+
